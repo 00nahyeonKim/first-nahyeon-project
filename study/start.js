463fd689ee692fd7c3181149fe6e6f78f1956c42
nahyeon Kim <nahyeon000918@gmail.com>
2023-11-14 22:15:37 +0900

    first commit

---
+++ study/start.js
@@ -0,0 +1,12 @@
+const readline = require('readline');
+
+const rl=readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+function init() {
+    readline.question('명령하세요 : ', (command) => {
+
+    });
+}
